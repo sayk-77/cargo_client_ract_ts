25f394f2f0612597671c4c56bbcfe87d27579640
Nikita <yawaihv1@gmail.com>
2024-02-06 14:23:01 +0300

    add page about us

--- src/router.tsx
+++ src/router.tsx
@@ -5,6 +5,7 @@ import { Contacts } from './Components/contacts/contacts';
 import { Question } from './Components/question/question';
 import { Authorization } from './Components/authorization/authorization';
 import { Registration } from './Components/registration/registration';
+import { About } from './Components/about/about';
 
 export const Router:React.FC = () => {
   return (
@@ -12,6 +13,7 @@ export const Router:React.FC = () => {
       <Routes>
         <Route path='/' element={<Home />}/>
         <Route path='/contacts' element={<Contacts />}/>
+        <Route path='/about' element={<About />} />
         <Route path='/ask-question' element={<Question />} />
         <Route path='/authorization' element={<Authorization />} />
         <Route path='/registration' element={<Registration />} />
